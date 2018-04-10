dd13c0fe7cb0d864559a1d2bf9bd60955c589db7
Lionel <zeadkiel13@gmail.com>
2018-04-10 10:46:21 +0200

    ajout de CUSTOM_ELEMENTS_SCHEMA a ngmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatCardModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA }      from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { BookService } from './book.service';
 import { AppComponent } from './app.component';
@@ -19,11 +20,12 @@ import { AppComponent } from './app.component';
 		FormsModule,
 		HttpClientModule,
 		MatButtonModule,
-		MatCheckboxModule
+		MatCheckboxModule,
 	],
 	providers: [
 		BookService
 	],
-	bootstrap: [AppComponent]
+	bootstrap: [AppComponent],
+    schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
